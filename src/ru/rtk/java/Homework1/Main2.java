381cfca43d6296c5c61c77ca7366ecefbf7a2148
Nani <dod328@gmail.com>
2025-07-15 13:45:04 +0700

    commit

---
+++ src/ru/rtk/java/Homework1/Main2.java
@@ -0,0 +1,19 @@
+package ru.rtk.java.Homework1;
+
+public class Main2 {
+    public static void main2(String[] args) {
+
+        doSomething();
+        String s = returnSomeStringh()
+
+
+        System.out.println(s);
+    }
+
+
+    public static void doSomething() {
+        System.out.println("do something");
+    }
+
+    public static String returnSomeStringh() {
+        System.out.println("do something");
